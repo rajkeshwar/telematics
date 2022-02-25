8e81a38b73e50c933528b143a8bf1e61bbaa9382
Rajkeshwar Prasad <rajkeshwarp@azuga.com>
2022-02-25 10:44:11 +0100

    asdf

--- public/telematics.js
+++ public/telematics.js
@@ -2,7 +2,7 @@
  * @Author: Rajkeshwar Prasad(rajkeshwar.pd@gmail.com) 
  * @Date: 2022-01-24 05:25:19 
  * @Last Modified by: Rajkeshwar Prasad
- * @Last Modified time: 2022-02-25 10:27:58
+ * @Last Modified time: 2022-02-25 10:44:01
  */
 
 class EvaluateForm {
@@ -10,7 +10,6 @@ class EvaluateForm {
     this.__formIndex = formIndex;
     this.serialize = this.serialize.bind(this);
     this.handleSubmit = this.handleSubmit.bind(this);
-    this.magic = new window.Magic('pk_live_2CE49584827D79B5'); // ✨
     this.fire();
   }
 
@@ -50,18 +49,6 @@ class EvaluateForm {
     })
       .then(resp => resp.json())
       .then(this.handleResponse)
-  }
-
-  async handleResponse(resp) {
-    console.log(`Received response :`, resp);
-    if (resp.hashlink) {
-      const email = resp['YOUR EMAIL'];
-      const redirectURI = `https://telematics.netlify.app?hashlink=${resp.hashlink}`;
-      const linkResponse = await this.magic.auth.loginWithMagicLink({ email, redirectURI });
-      alert('Your request is send. Please check your email.');
-      window.__linkReponse = linkResponse;
-      console.log(linkResponse)
-    }
   }
 
   serialize(form) {
